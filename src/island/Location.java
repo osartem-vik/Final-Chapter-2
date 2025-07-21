f7a525356b3e451180529412a420a9fd04af2111
”Artem <26and06@gmail.com>
2025-07-21 13:57:19 +0300

    was made a class of location

---
+++ src/island/Location.java
@@ -0,0 +1,44 @@
+package island;
+
+import models.Creature;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Location {
+    private final List<Creature> creatures;
+    private final int maxCapacity;
+
+    public Location(int maxCapacity) {
+        this.creatures = new CopyOnWriteArrayList<>();
+        this.maxCapacity = maxCapacity;
+    }
+
+    public boolean addCreature(Creature creature) {
+        if (creature != null && creature.isAlive() && creatures.size() < maxCapacity) {
+            return creatures.add(creature);
+        }
+        return false;
+    }
+
+    public boolean removeCreature(Creature creature) {
+        return creature != null && creatures.remove(creature);
+    }
+
+    public List<Creature> getCreatures() {
+        return new ArrayList<>(creatures); // Повертаємо копію для безпечного доступу
+    }
+
+    public int getCreatureCount() {
+        return creatures.size();
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public boolean isFull() {
+        return creatures.size() >= maxCapacity;
+    }
+}

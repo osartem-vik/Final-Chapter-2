b9b36d87c6035b7abb1a02d48fc7ec63e061785e
”Artem <26and06@gmail.com>
2025-07-21 13:37:23 +0300

    was made a main class

---
+++ src/IslandMain.java
@@ -0,0 +1,8 @@
+import engine.Simulation;
+
+public class IslandMain {
+    public static void main(String[] args) {
+        Simulation simulation = new Simulation();
+        simulation.start();
+    }
+}
